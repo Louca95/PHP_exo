aeab8570199b2a64b9b1bd8b22eaafbfe575a6c7
Louca cascalheira-mercier <cascalheiralouca@gmail.com>
2024-10-25 10:54:35 +0200

    exo_php

--- traitement.php
+++ traitement.php
@@ -9,20 +9,51 @@
  *  1 . S'assurer que tous les champs sont remplis avant d'afficher les données
  *  $nom = isset($_POST['nom']) ? $_POST['nom'] : 'Nom non fourni';
            
- */
- $prenom = isset($_POST['prenom']) ? $_POST['prenom'] : 'Prénom non fourni';
+ */if(empty($_POST['firstname']) && empty($_POST['nom']) && empty($_POST['email']) && empty($_POST['mdp']) && empty($_POST['city']) && empty($_POST['country'])){
+    echo "champ a remplir";
+ }else{
+    echo  $_POST['firstname'];
+    echo  $_POST['nom'];
+    echo  $_POST['email'];
+    echo  $_POST['mdp'];
+    echo  $_POST['city'];
+    echo  $_POST['country'];
+ }
+ $prenom = isset($_POST['firstname']) ? $_POST['prenom'] : 'Prénom non fourni';
  $nom = isset($_POST['nom']) ? $_POST['nom'] : 'Nom non fourni';
- $eamil = isset($_POST['email']) ? $_POST['email'] : 'Email non fourni';
- $password = isset($_POST['password']) ? $_POST['password'] : 'mot de passe non fourni';
- $nom = isset($_POST['town']) ? $_POST['town'] : 'Ville non fourni';
- $nom = isset($_POST['pays']) ? $_POST['pays'] : 'Pays non fourni';
+ $email = isset($_POST['email']) ? $_POST['email'] : 'Email non fourni';
+ $password = isset($_POST['mdp']) ? $_POST['password'] : 'mot de passe non fourni';
+ $town = isset($_POST['city']) ? $_POST['town'] : 'Ville non fourni';
+ $pays = isset($_POST['country']) ? $_POST['pays'] : 'Pays non fourni';
 
 /** Exercice 2 : traitement et affichage sécurisé
  * 
  *  Objectif : Afficher les données du formulaire de manière sécurisée pour les éviter les attaques XSS (échapper les données) (possibilité de copier/coller le traitement de l'exercice 1)
  * 
  */
+echo "<br>";
+if(empty($_POST['firstname']) && empty($_POST['nom']) && empty($_POST['email']) && empty($_POST['mdp']) && empty($_POST['city']) && empty($_POST['country'])){
+    echo "champ a remplir";
+ }else{
+    echo  htmlspecialchars($_POST['firstname']);
+    echo  htmlspecialchars($_POST['nom']);
+    echo  htmlspecialchars($_POST['email']);
+    echo  htmlspecialchars($_POST['mdp']);
+    echo  htmlspecialchars($_POST['city']);
+    echo  htmlspecialchars($_POST['country']);
+}
 
+// Récupération des données POST 
+        $nom = $_POST['nom'];
+        $prenom = $_POST[''];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $town = $_POST['town'];
+        $pays = $_POST['pays'];
+
+        // Affichage des données
+        echo "Nom (POST): " . htmlspecialchars($nom) . "<br>";
+        echo "Prénom (POST): " . htmlspecialchars($prenom) . "<br>";
 
 /** Exercice 3 : Afficher les données précédentes
  * 
@@ -34,28 +65,14 @@
  * 
  */
 
-        // Récupération des données POST
-        $nom = $_POST['nom'];
-        $prenom = $_POST['prenom'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $town = $_POST['town'];
-        $pays = $_POST['pays'];
+        
 
-        // Affichage des données
-        echo "Nom (POST): " . htmlspecialchars($nom) . "<br>";
-        echo "Prénom (POST): " . htmlspecialchars($prenom) . "<br>";
-
-$email =200;
+$email = 200;
 $town = 130;
 
-if ($_POST['email'] === 200) {
-        echo "<p>email vide</p>";
-    }else {
-       echo "vous ne pouvez pas entrez cette adresse mail";
-    }
 
-if ($_POST['town'] === 130) {
+
+if ($_POST['town'] = 130) {
         echo "<p>town vide</p>";
     }else {
        echo "vous ne pouvez pas entrez cette adresse";
@@ -66,7 +83,12 @@ if ($_POST['nom'] && $_POST['prenom'] && $_POST['email'] && $_POST['password'] &
     echo '<h1>Résultats du formulaire</h1>';
     echo '<p>Nom : ' . htmlspecialchars($_POST['nom']) . '</p>';
     echo '<p>Prénom : ' . htmlspecialchars($_POST['prenom']) . '</p>';
-    echo '<p>Email : ' . htmlspecialchars($_POST['email']) . '</p>';
+    if ($_POST['email'] = 200) {
+        echo "<p>email vide</p>";
+    }else {
+       echo "vous ne pouvez pas entrez cette adresse mail";
+    }
+    
     echo '<p>Password : ' . htmlspecialchars($_POST['password']) . '</p>';
     echo '<p>Town : ' . htmlspecialchars($_POST['town']) . '</p>';
     echo '<p>Pays : ' . htmlspecialchars($_POST['pays']) . '</p>';
